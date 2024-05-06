5f483f33cb534bd57dde7a9aae4855c5e53f42c2
Amit Vikram Raj <avr13405@gmail.com>
2024-05-06 18:37:27 +0530

    adding check-code-quality.sh and modifiying git pre-commit hook on local to run it while code commiting

---
+++ check-code-quality.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+EXIT_STATUS=0
+
+
+black --config .black.toml code-quality/type-hints/ || ((EXIT_STATUS++))
+pylint --rcfile .pylintrc *.py code-quality/type-hints/*.py || ((EXIT_STATUS++))
+flake8 --config .flake8 code-quality/type-hints/ || ((EXIT_STATUS++))
+mypy code-quality/type-hints/ --exclude .venv || ((EXIT_STATUS++))
+ruff check code-quality/type-hints/ --config ruff.toml --fix || ((EXIT_STATUS++))
+isort code-quality/type-hints/ --settings .isort.cfg || ((EXIT_STATUS++))
+
+exit $EXIT_STATUS
